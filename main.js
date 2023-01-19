badc53b570263baad3af40b932e6416a774c7073
MORGANPOSADA <20213tn035@utez.edu.mx>
2023-01-19 00:07:48 -0600

    new version bro

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+const form = document.querySelector("#form");
+const input =document.querySelector("#search");
+const paste = document.querySelector("#gifts");
+const END_POINT_SEARCH = "https://api.giphy.com/v1/gifs/search";
+const END_POINT_RANDOM = "https://api.giphy.com/v1/gifs/random";
+const MY_API_KEY = "LubDDF2EJwRpbDJy6aHJpc1NbUAP5WSa";  
+
+
+
+
+const showGifts = async (e) =>{
+    e.preventDefault();
+    console.log(input.value);
+
+    const URL = `${END_POINT_SEARCH}?api_key=${MY_API_KEY}&q=${input.value}`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data)
+    let template= '';
+    data.data.forEach(element => {
+        const {url} = element.images.original;
+
+        /*const{images} = gif;
+        const{original}= images
+        const{url}=original
+        */
+
+        template+= `<img width="100px" height="100px" src="${url}"/>`
+
+
+    });
+
+    paste.innerHTML =template
+
+
+}
+form.addEventListener("submit", showGifts);
+
+
+
+
+
+
+
+
+
+/*
 const url ='https://api.giphy.com/v1/gifs/search';
 let busqueda = "?q=";
 const key ='&api_key=LubDDF2EJwRpbDJy6aHJpc1NbUAP5WSa';
@@ -7,7 +54,7 @@ let q="";
 
 let urlCompleta ="";
 //urlCompleta = url + busqueda + q + key + limite; //esta se supone es mi url completa, puede cambiar o variar XD
-const btn = document.getElementById('btn');  /*Se tiene acceso al boton */
+const btn = document.getElementById('btn');  s
 
 btn.onclick = () =>{
     document.getElementById("portfolio").innerHTML ="";
@@ -35,3 +82,4 @@ const getData = async() =>{
 
 getData();
 
+*/
